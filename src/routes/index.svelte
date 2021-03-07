d049c6c7d1df2a691afe9499563f5c8ecfa3d406
Geoff <git@geoffdick.com>
2021-03-07 11:20:32 -0700

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+	import ExampleComponent from "../components/ExampleComponent.svelte";
+</script>
+
+<style>
+	.centerer {
+		@apply flex-1 flex flex-col items-center justify-center bg-dark;
+	}
+</style>
+
+<div class:centerer={true}> <!-- Here's how you could extract conditional class groups with @apply -->
+	<ExampleComponent
+		title="🍗 TendieCalc"
+		paragraph="How many tendies???" />
+</div>
