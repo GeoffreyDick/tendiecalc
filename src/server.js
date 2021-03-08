ce61eb63d258cefbf5c7ff8cb0583d7e0f2ac5a2
Geoff <git@geoffdick.com>
2021-03-08 05:45:25 -0700

    🐛 fix: Fix Vercel deploy error Pt. 8

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 import sirv from "sirv";
-import Express from "express";
+import express from "express";
 import compression from "compression";
 import * as sapper from "@sapper/server";
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
-export default Express()
+const app = express()
 	.use(
 		compression({ threshold: 0 }),
 		sirv("static", { dev }),
 		sapper.middleware(),
-	)
-	.listen(PORT, (err) => {
-		if (err) console.log("error", err);
-	});
+	);
+
+app.listen(PORT, (err) => {
+	if (err) console.log("error", err);
+});
+
+export default app;
